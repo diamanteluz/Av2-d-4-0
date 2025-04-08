63c4a1fa3c5888f7ace6c68d010d3bbfa1d32715
Isadora Silgueiro Nunes Pereira <145803856+diamanteluz@users.noreply.github.com>
2025-04-08 08:37:37 -0300

    we

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-15-05T00:00:00");
+const tempoObjetivo1 = new Date("2025-05-15T00:00:00");
 const tempoObjetivo2 = new Date("2025-12-19T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-17T00:00:00");
 const tempoObjetivo4 = new Date("2025-12-10T00:00:00");
